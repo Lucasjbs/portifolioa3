d2e6d5fd962e8b428a308ec9f17c3fe4775a2722
Lucas J Bastos <lucasjbastos26@gmail.com>
2024-03-13 20:24:43 -0300

    refactor: client server split

--- client/autoload.php
+++ client/autoload.php
@@ -1,7 +1,7 @@
 <?php
 
 spl_autoload_register(function (string $nomeClasse){
-    $filePath = str_replace('Portifolio\\Workbench', __DIR__ . DIRECTORY_SEPARATOR . 'src', $nomeClasse);
+    $filePath = str_replace('Portifolio\\Workbench', __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'server' . DIRECTORY_SEPARATOR . 'src', $nomeClasse);
     $filePath = str_replace('\\', DIRECTORY_SEPARATOR, $filePath);
     $filePath .= '.php';
     
